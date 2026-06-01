f0a735e5530ae493e2a5ae093da2a4f3a4bc7b7f
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-06-01 22:58:32 +0530

    authComplite

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -21,7 +21,32 @@ export const signup = async (req,res) => {
           }
           const user = await User.findOne({email:email})
           if(user) return res.ststus(400).json({message: "user already exists"} )
+
+               //123456 =>$dnjasdkas_?knrkjfb
+               const salt = await bcrypt.genSalt(10)
+               const hashedPassword = await bcrypt.hash(Password,salt)
+
+               const newUser = new User({
+                    fullName,
+                    email,
+                    password: hashedPassword
+               })
+               if(newUser){
+                    generateToken(newUser._id, res)
+                    await newUser.save()
+                    res.status(201).json({
+                         _id: newUser._id,
+                         fullName: newUser.fullName,
+                         email: newUser.email,
+                         profilePic: newUser.profilePic
+                    });
+
+               }else {
+                    res.ststus(400).json({message: "invalid user data"})
+               }
+          
      }catch(error){
-        
+        console.log("Error in signup controller", error);
+        res.status(500).json({message: "Internal server error"});
      } 
 }
