1ab1c2e33dd8293d61de649a23e3ee1cb7360986
Mayankupadhyay-25 <155510307+Mayankupadhyay-25@users.noreply.github.com>
2026-06-02 23:44:37 +0530

    error

--- server/src/routes/auth.route.js
+++ server/src/routes/auth.route.js
@@ -3,13 +3,14 @@ import { signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.post("/signup",signup);
+router.post("/signup", signup);
 
-router.get("/login",(req,res) => {
+router.post("/login", (req, res) => {
     res.send("Login endpoint");
 });
-router.get("/logout",(req,res) => {
+
+router.post("/logout", (req, res) => {
     res.send("Logout endpoint");
 });
 
-export default router 
+export default router;
--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import Path from "path";
+import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
@@ -13,23 +14,23 @@ const __dirname = Path.resolve();
 
 const PORT = process.env.PORT || 3000;
 
-
+app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for the deployment
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(Path.join(__dirname,"../client/dist")));
-    
-    app.get("*",(req,res) => {
-        res.sendFile(Path.join(__dirname, "../client", "dist", "index.html"))
-    })
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(Path.join(__dirname, "../client/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(Path.join(__dirname, "../client", "dist", "index.html"));
+    });
 }
 
-app.listen(PORT, ()=>{
-     console.log('Server is running on port: ' + PORT)
-     connectDB()
-})
+app.listen(PORT, () => {
+    console.log('Server is running on port: ' + PORT);
+    connectDB();
+});
 
 export default app;
